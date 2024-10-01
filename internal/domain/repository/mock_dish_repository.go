16e43c6d6efe1a340f6b951ac98983596402cf0d
Alex Perez Garcia <aperezgdev@gmail.com>
2024-10-01 21:24:50 +0200

    test: add mock repositories for testing

---
+++ internal/domain/repository/mock_dish_repository.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"github.com/stretchr/testify/mock"
+
+	"github.com/aperezgdev/food-order-api/internal/domain/model"
+	dish_vo "github.com/aperezgdev/food-order-api/internal/domain/value_object/dish"
+)
+
+type MockDishRepository struct {
+	mock.Mock
+}
+
+func (m *MockDishRepository) FindAll() ([]model.Dish, error) {
+	args := m.Called()
+
+	return args.Get(0).([]model.Dish), args.Error(1)
+}
+
+func (m *MockDishRepository) Save(dish model.Dish) error {
+	args := m.Called(dish)
+	return args.Error(1)
+}
+
+func (m *MockDishRepository) Find(id dish_vo.DishId) (model.Dish, error) {
+	args := m.Called(id)
+
+	return args.Get(0).(model.Dish), args.Error(1)
+}
+
+func (m *MockDishRepository) Update(dish model.Dish) error {
+	args := m.Called(dish)
+
+	return args.Error(1)
+}
+
+func (m *MockDishRepository) Delete(id dish_vo.DishId) error {
+	args := m.Called(id)
+
+	return args.Error(0)
+}
