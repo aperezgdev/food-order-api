623b3d511f51aa1dfc41314f0a00fd56c77d295c
Alex Perez Garcia <aperezgdev@gmail.com>
2024-10-01 21:26:29 +0200

    test: testing for user application

--- internal/application/User/user_creator_test.go
+++ internal/application/User/user_creator_test.go
@@ -4,19 +4,19 @@ import (
 	"log/slog"
 	"testing"
 
+	"github.com/stretchr/testify/mock"
+
 	"github.com/aperezgdev/food-order-api/internal/domain/model"
+	"github.com/aperezgdev/food-order-api/internal/domain/repository"
 	vo "github.com/aperezgdev/food-order-api/internal/domain/shared/value_object"
 	value_object "github.com/aperezgdev/food-order-api/internal/domain/value_object/user"
-	"github.com/aperezgdev/food-order-api/internal/infrastructure/repository"
 )
-
-func newTestUserCreator() *UserCreator {
-	return NewUserCreator(repository.NewUserInMemoryRepository(), slog.Default())
-}
 
 // Should create a user without error
 func TestUserCreator(t *testing.T) {
-	userCreator := newTestUserCreator()
+	userRepository := repository.NewMockUserRepository()
+	userRepository.On("Save", mock.Anything).Return(nil)
+	userCreator := NewUserCreator(userRepository, slog.Default())
 
 	user := model.User{
 		Id:        value_object.UserId("2"),
@@ -34,31 +34,5 @@ func TestUserCreator(t *testing.T) {
 
 	if testError != nil {
 		t.Errorf("TestUserCreator - Error has ocurred trying to create a user")
-	}
-}
-
-// Should create and save a user
-func TestUserCreatorAndSave(t *testing.T) {
-	userRepository := repository.NewUserInMemoryRepository()
-	userCreator := NewUserCreator(userRepository, slog.Default())
-	userFinder := NewUserFinder(userRepository, slog.Default())
-
-	user := model.User{
-		Id:        value_object.UserId("2"),
-		Name:      value_object.UserName("John"),
-		Email:     value_object.UserEmail("john@john.com"),
-		CreatedOn: vo.NewCreatedOn(),
-	}
-
-	userCreator.Run(&user)
-
-	var testError error
-	result := userFinder.Run(value_object.UserId("2"))
-	result.Error(func(err error) {
-		testError = err
-	})
-
-	if testError != nil {
-		t.Errorf("TestUserCreatorAndSave - User was not found")
 	}
 }
--- internal/application/User/user_finder.go
+++ internal/application/User/user_finder.go
@@ -1,13 +1,10 @@
 package application
 
 import (
-	"database/sql"
-	"errors"
 	"log/slog"
 
 	"github.com/aperezgdev/food-order-api/internal/domain/model"
 	"github.com/aperezgdev/food-order-api/internal/domain/repository"
-	domain_errors "github.com/aperezgdev/food-order-api/internal/domain/shared/domain_error"
 	result "github.com/aperezgdev/food-order-api/internal/domain/shared/result"
 	value_object "github.com/aperezgdev/food-order-api/internal/domain/value_object/user"
 )
@@ -24,13 +21,8 @@ func NewUserFinder(userRepository repository.UserRepository, slog *slog.Logger) 
 func (uf *UserFinder) Run(id value_object.UserId) *result.Result[model.User] {
 	uf.slog.Info("UserFinder.Run - User Id", slog.Any("id", id))
 	user, err := uf.userRepository.FindById(id)
-
-	if err != nil && errors.Is(err, sql.ErrNoRows) {
-		return result.ErrorResult[model.User](domain_errors.NotFound)
-	}
-
 	if err != nil {
-		return result.ErrorResult[model.User](domain_errors.Database)
+		return result.ErrorResult[model.User](err)
 	}
 
 	return result.OkResult(&user)
